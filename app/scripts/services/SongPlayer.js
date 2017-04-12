ca5971aa8e5d6dc031c6d9381a3295dab24372ca
Andrea <apmansur1993@gmail.com>
2017-04-12 10:12:40 -0500

    add documentation and fixed playSong

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,61 +1,82 @@
- (function() {
-     function SongPlayer() {
-         var SongPlayer = {};
-         
-         var currentSong = null;
-         var currentBuzzObject = null;
-         
-         /**
-         * @function setSong
-         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-         * @param {Object} song
-         */
-         
-         var setSong = function(song) {
-          if (currentBuzzObject) {
-             currentBuzzObject.stop();
-             currentSong.playing = null;
-         }
-             
-         /**
-         * @desc Buzz object audio file
-         * @type {Object}
-         */
 
-         currentBuzzObject = new buzz.sound(song.audioUrl, {
-             formats: ['mp3'],
-             preload: true
-         });
- 
-         currentSong = song;
+(function() {
+    function SongPlayer() {
+        var SongPlayer = {};
+        /**
+        * @desc Holds current song number
+        * @type {Number}
+        */
+        var currentSong = null;
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
+        var currentBuzzObject = null
+        
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+            
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+            
+            currentSong = song;
         };
-
-
-         SongPlayer.play = function(song) {
-             if (currentSong !== song) {
-                 setSong(song);
-                 currentBuzzObject.play();
-                 song.playing = true;
-             }
-             else if (currentSong === song) {
-                     if (currentBuzzObject.isPaused()) {
-                         currentBuzzObject.play();
-                     }
-             }  
-         };
-          return SongPlayer;
-     };
-     
-     SongPlayer.pause = function(song) {
-        currentBuzzObject.pause();
-        song.playing = false;
-    };
- 
-     angular
-         .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
- })();
-
+        
+        /**
+        * @function playSong
+        * @desc Plays current song and sets song.playing to true
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        /**
+        * @function SongPlayer.play
+        * @desc Plays current song if paused, or selected song
+        * @param {Object} song
+        */
+        SongPlayer.play = function(song) {
+            if (currentSong !== song) {
+                
+                setSong(song);
+                playSong(song);
+                
+            } else if (currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    playSong(song);
+                }
+            }
+        };
+        
+        /**
+        * @function SongPlayer.pause
+        * @desc Pauses current song
+        * @param {Object} song
+        */
+        SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        };
+        
+        return SongPlayer;
+    }
+    
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+})();
 
     
         
